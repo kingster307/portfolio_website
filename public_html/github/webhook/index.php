b2179d0b97d8b4fa34c107cd82cebc853267f6fd
Peter King <techking61@outlook.com>
2019-09-24 22:46:28 -0400

    clean up comments & structure

--- public_html/github/webhook/index.php
+++ public_html/github/webhook/index.php
@@ -1,57 +1,38 @@
 <?php
 
-#this gets thes payload request 
-$data=json_decode(file_get_contents('php://input'),1);
+#payload 
+$data = json_decode(file_get_contents('php://input'), 1);
 
-#find URL from payload
-#$data["repository"]["html_url"]
-
-#find branch
-#explode("/",$data["ref"])[2]
-
-#find if post 
-#$_SERVER['REQUEST_METHOD']
-
-function verify($reqHead){
+# needs work
+function verify($reqHead)
+{
   #test http method
-  if($reqHead['REQUEST_METHOD'] == "POST")
-  {
-   #need to check secret
-   return true;
+  if ($reqHead['REQUEST_METHOD'] == "POST") {
+    #need to check secret for added security
+    return true;
   }
- }
-
+}
 
 function run($verified, $data)
- {
-  if($verified == true)
-  {
-   #find branch
-   # echo(explode("/",$data["ref"])[2]);
-    $branch = explode("/",$data["ref"])[2];
-   #take in URL
-   # $url ="https://github.com/kingster307/portfolio_website.git";
-   #take in git action
-    $action = "pull "; 
-   #put it all in array
+{
+  if ($verified == true) {
+    #find branch
+    $branch = explode("/", $data["ref"])[2];
+    #Set git action
+    $action = "pull ";
+    #create command string
     $commands = "git " . $action . "ghubpull " . $branch;
-   #verify branch (live site only for master)
-    if($branch == "master")
-    {
-     #run shell commands line 40
-     $output = shell_exec($commands); 
-    #printf("output " . $output);
-    }else{
-     echo("wrong branch ");    
-    }    
-  }else{
-   echo("not verified");
+    #verify branch (live site only for master)
+    if ($branch == "master") {
+      #run shell commands
+      $output = shell_exec($commands);
+    } else {
+      echo ("branch error");
+    }
+  } else {
+    echo ("not verified");
   }
-
- }
+}
 
 #entry point
 run(verify($_SERVER), $data);
-
-?>
-
